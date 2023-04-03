7b872e1f198f3b80953b5e49736150c17c2f7464
Raja <35105269+IamRajaSwar@users.noreply.github.com>
2023-04-03 17:41:51 +0530

    docker project

---
+++ src/main/java/com/csd/ms/docker/DockerDemoApplication.java
@@ -0,0 +1,19 @@
+package com.csd.ms.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+	@GetMapping("/docker-demo")
+	public String hello() {
+		return "Welcome to docker! 🤩🤩";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+}
